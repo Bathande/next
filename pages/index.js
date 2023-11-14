670c1a61fe83cb5c4f38df92f719e42590bd7927
Bathande Ndaba <bathande704@gmail.com>
2023-11-14 11:57:59 +0200

    Please enter the commit message for your changes. Lines starting

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import { Amplify, API } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+Amplify.configure({ ...awsExports, ssr: true });
+
 export default function Home() {
   return (
     <div >
